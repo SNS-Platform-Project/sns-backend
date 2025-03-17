e34fc2cfa0cec7d0f4bc0d220417d6e4e64bad08
minyoung555 <mindud02@naver.com>
2025-03-17 18:43:12 +0900

    수정: 코드 정리

--- src/main/java/com/example/snsbackend/config/EmailConfig.java
+++ src/main/java/com/example/snsbackend/config/EmailConfig.java
@@ -1,13 +1,11 @@
 package com.example.snsbackend.config;
 
-import com.example.snsbackend.email.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.util.Properties;
 
--- src/main/java/com/example/snsbackend/dto/RegisterRequest.java
+++ src/main/java/com/example/snsbackend/dto/RegisterRequest.java
@@ -2,7 +2,6 @@ package com.example.snsbackend.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
